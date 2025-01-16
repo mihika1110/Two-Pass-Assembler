9521bee37c0cd729a6401bc7d3cac080ace52b69
mihika1110 <163761030+mihika1110@users.noreply.github.com>
2025-01-16 23:34:41 +0530

    Create product.asm

---
+++ product.asm
@@ -0,0 +1,7 @@
+
+; Array elements
+array_base:  data 4            ; array[0] = 1
+             data 2            ; array[1] = 2
+             data 3            ; array[2] = 3
+             data 4            ; array[3] = 4
+             data 5            ; array[4] = 5
